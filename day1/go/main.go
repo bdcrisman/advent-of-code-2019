2f0b2c1f742ebe4bd7dbaedbde4318c633cd8d88
Ben Crisman <benjamin.crisman@gmail.com>
2019-12-15 00:37:48 -0700

    day1: go correct answer

--- day1/go/main.go
+++ day1/go/main.go
@@ -4,20 +4,50 @@ import (
 	"bufio"
 	"log"
 	"os"
+	"strconv"
+	"sync"
 )
 
+// SafeSum is a mutex wrapper for the overall sum.
+type SafeSum struct {
+	sum int
+	mux sync.Mutex
+}
+
+// AddToSum safely adds to the overall sum.
+func (s *SafeSum) AddToSum(value int) {
+	s.mux.Lock()
+	s.sum += value
+	s.mux.Unlock()
+}
+
+// For fun, I'm utilizing a goroutine and waitgroup to sum the
+// fuel requirements from the input file.
+// I guess I could do this one line at a time and it would probably
+// take less time...but I'll have to time my solution.
 func main() {
-	inputs, err := getInput("../input")
+	inputArr, err := getInputData("../input")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	for _, data := range inputs {
-		println(data)
+	s := SafeSum{}
+	var wg sync.WaitGroup
+	wg.Add(len(inputArr))
+
+	for _, input := range inputArr {
+		num, err := strconv.Atoi(input)
+		if err != nil {
+			continue
+		}
+		go getFuelRequirement(num, &s, &wg)
 	}
+
+	wg.Wait()
+	println(s.sum)
 }
 
-func getInput(path string) (lines []string, err error) {
+func getInputData(path string) (lines []string, err error) {
 	f, err := os.Open(path)
 	if err != nil {
 		return
@@ -31,4 +61,9 @@ func getInput(path string) (lines []string, err error) {
 
 	err = scanner.Err()
 	return
+}
+
+func getFuelRequirement(mass int, s *SafeSum, wg *sync.WaitGroup) {
+	defer wg.Done()
+	s.AddToSum(int(mass/3) - 2)
 }
